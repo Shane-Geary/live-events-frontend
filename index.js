d4da6d39700fa3c48a51c9bfcd58ed1a8ab80009
Gearz <shanegeary@yahoo.com>
2021-02-08 14:28:15 -0700

    Implemented get fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/tickets";
+
+document.addEventListener('DOMContentLoaded', () => {
+    getTickets()
+})
+
+function getTickets() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(tickets => {
+        tickets.data.forEach(ticket => {
+        const ticketMarkup = `
+            <div data-id=${ticket.id}>
+                <h2>${ticket.attributes.title}</h2>
+                <h3>${ticket.attributes.date}</h3>
+                <h3>${ticket.attributes.main_act}</h3>
+                <p>${ticket.attributes.category.name}</p>
+                <button data-id=${ticket.id}>edit</button>
+            </div>
+            <br><br>`;
+
+            document.querySelector('#ticket-container').innerHTML += ticketMarkup
+        })
+    }) 
+} 
