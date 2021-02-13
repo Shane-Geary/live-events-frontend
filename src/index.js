547808797eeaf66456e043bc9bfd6f36f77d7aab
Gearz <shanegeary@yahoo.com>
2021-02-13 08:05:26 -0700

    Changed mind on edit feature for now. changed back code

--- src/index.js
+++ src/index.js
@@ -4,15 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     getTickets()
 
     const ticketForm = document.querySelector("#create-ticket-form")
-    ticketForm.addEventListener("submit", (e) => formHandler(e));
 
-    const ticketContainer = document.querySelector("#ticket-container")
-    ticketContainer.addEventListener("click", e => {
-        const id = parseInt(e.target.dataset.id);
-        const ticket = Ticket.findById(id);
-        document.querySelector("#update-ticket").innerHTML = ticket.renderUpdateForm();
-    });
-    document.querySelector("#update-ticket").addEventListener("submit", e => updateForm(e))
+    ticketForm.addEventListener("submit", (e) => formHandler(e));
 })
 
 function getTickets() {
@@ -53,14 +46,3 @@ function postFetch(title, date, main_act, category_id) {
         document.querySelector('#ticket-container').innerHTML += newTicket.renderTicket()
     })
 }
-
-function updateForm(e) {
-    e.preventDefault();
-    const id = parseInt(e.target.dataset.id);
-    const ticket = Ticket.findById(id);
-    const title = e.target.querySelector('#input-title').value;
-    const date = e.target.querySelector('#input-date').value;
-    const main_act = e.target.querySelector('#main-act').value;
-    const category_id = parseInt(e.target.querySelector('#categories').value);
-    patchSyllabus(ticket, title, date, main_act, category_id)
-  }
--- src/ticket.js
+++ src/ticket.js
@@ -16,43 +16,12 @@ class Ticket {
                     <h3>Date: ${this.date}</h3>
                     <h3>Main Act: ${this.main_act}</h3>
                     <p>Category: ${this.category.name}</p>
-                    <button data-id=${this.id}>edit</button>
                 </div>
                 <br><br>`;
     }
 
     static findById(id) {
         return this.all.find(ticket => ticket.id === id);
-    }
-
-    renderUpdateForm() {
-        return `
-        <form data-id=${this.id} >
-          <h3>Edit this Ticket</h3>
-    
-          <label>Title</label>
-          <input id='input-title' type="text" name="title" value="${this.title}" class="input-text">
-          <br><br>
-    
-          <label>Date</label>
-          <input id='input-date' name="date" value="">${this.date}</input>
-          <br><br>
-    
-          <label>Main Act</label>
-          <input id='input-main-act' type="text" name="main_act" value="${this.main_act}" class="input-text">
-          <br><br>
-    
-          <label>Category</label>
-          <select id="categories" name="categories" value="${this.category.name}">
-            <option value="1">Concert</option>
-            <option value="2">Sport Event</option>
-            <option value="3">Theatre</option>
-          </select>
-          <br><br>
-    
-          <input id='edit-button' type="submit" name="submit" value="Edit Ticket" class="submit">
-        </form>
-      `;
     }
 }
 
